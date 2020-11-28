70075c96e066a036e5769f34d04d379a1df816aa
Dogukan Gaffaroglu <dogukangaffaroglu@gmail.com>
2020-11-28 20:52:10 +0300

    Set the port number from the environment variable  again

--- src/main/java/bil481hw1/App.java
+++ src/main/java/bil481hw1/App.java
@@ -13,7 +13,11 @@ import java.util.Map;
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
+
+
+import static spark.Spark.get;
 import static spark.Spark.port;
+import static spark.Spark.post;
 
 import java.util.*;
 public class App {
